30f3f16ab4807813dafec1edfb7a528c83a68ef8
dzulfilove <dzulfivector@gmail.com>
2023-05-04 02:38:11 -0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+fetch('https://64527375bce0b0a0f7475dda.mockapi.io/blog-posts')
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+
+    data.forEach((item,index)=> {
+        const titleId= 'title' + (index + 1);
+        const textId= 'text' + (index + 1);
+        const imgId= 'img' + (index + 1);
+        const catId= 'category' + (index + 1);
+
+        document.getElementById(titleId).innerHTML =item.title;
+        document.getElementById(textId).innerHTML =item.text;
+        document.getElementById(imgId).src = item.image;
+        document.getElementById(catId).innerHTML = item.category;
+    }
+    )
+
+
+
+})
+.catch(error => console.log(error))
+
 fetch('https://64527770a2860c9ed40d2a69.mockapi.io/doctor')
 .then(response => response.json())
 .then(data => {
